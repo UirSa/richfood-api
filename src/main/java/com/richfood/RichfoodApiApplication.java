fda5de3f90e0171a7a683809303f26567383523b
UirSa <cout850729@gmail.com>
2024-12-30 16:20:01 +0800

    202412300418

---
+++ src/main/java/com/richfood/RichfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.richfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichfoodApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RichfoodApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/richfood/RichfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RichfoodApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
