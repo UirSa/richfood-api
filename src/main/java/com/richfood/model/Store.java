89f1ff61ff5fb1cf336b6396db0eecdd474d409f
UirSa <cout850729@gmail.com>
2025-01-05 22:15:29 +0800

    add Store Model

---
+++ src/main/java/com/richfood/model/Store.java
@@ -0,0 +1,54 @@
+package com.richfood.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer storeId;
+
+    private Integer restaurantId;
+    private String userName;
+    private String password;
+
+    public Integer getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(Integer storeId) {
+        this.storeId = storeId;
+    }
+
+    public Integer getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(Integer restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    //------------------------------------
+    //TODO 寫與餐廳表J
+
+}
