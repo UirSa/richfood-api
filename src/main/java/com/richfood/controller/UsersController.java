b9a0d3b05f9afad20f05d3fd7cb99e668deb6cc2
idreamtown <idreamtownship@gmail.com>
2025-01-07 21:03:06 +0800

    UsersNochange

--- src/main/java/com/richfood/controller/UsersController.java
+++ src/main/java/com/richfood/controller/UsersController.java
@@ -25,7 +25,7 @@ import com.richfood.service.UsersService;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/ ")
 public class UsersController {
 	
 	@Autowired UsersService userService;
--- src/main/java/com/richfood/util/BCrypt.java
+++ src/main/java/com/richfood/util/BCrypt.java
@@ -12,7 +12,7 @@
 // ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-package com.richfood.util;
+package com.richfood.util; 
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
