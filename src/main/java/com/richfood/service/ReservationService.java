d8877183059c8e6a354c67a280753d0df9a1fa77
djspchen <djspchen@gmail.com>
2025-01-08 15:52:41 +0800

    "2025-01-08"

---
+++ src/main/java/com/richfood/service/ReservationService.java
@@ -0,0 +1,40 @@
+package com.richfood.service;
+
+import java.time.OffsetTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.richfood.model.Reservations;
+import com.richfood.repository.ReservationRepository;
+
+@Service
+public class ReservationService {
+	@Autowired
+	private ReservationRepository reservationRepository;
+	
+    public Reservations addSeat(Reservations reservation) {
+        // 自動設置 editTime 為當前時間（+00）
+        reservation.setEditTime(OffsetTime.now());
+        
+        boolean exists = reservationRepository.existsByUserIdAndReservationDateAndReservationTimeAndStoreId(
+                reservation.getUserId(), 
+                reservation.getReservationDate(),  // 使用 Date 類型
+                reservation.getReservationTime(),  // 使用 Time 類型
+                reservation.getStoreId()
+            );
+            
+            if (exists) {
+                // 若已存在，拋出異常
+                throw new IllegalArgumentException("相同的預約已存在，無法新增");
+            }
+
+            // 若不存在相同預約，則保存新的預約
+            return reservationRepository.save(reservation);
+    }
+    public void deleteSeat(Integer reservationId) {
+    	reservationRepository.deleteById(reservationId);
+    }
+
+	
+}
