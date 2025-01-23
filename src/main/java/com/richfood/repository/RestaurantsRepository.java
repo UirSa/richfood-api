ef9fcb44989638139eb265672417d2ed17f78307
UirSa <cout850729@gmail.com>
2025-01-23 16:31:37 +0800

    test: findRestautantsByKetword

--- src/main/java/com/richfood/repository/RestaurantsRepository.java
+++ src/main/java/com/richfood/repository/RestaurantsRepository.java
@@ -2,9 +2,11 @@ package com.richfood.repository;
 
 
 import com.richfood.model.Restaurants;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -20,6 +22,13 @@ public interface RestaurantsRepository extends JpaRepository<Restaurants, Intege
     Page<Restaurants> findRestaurantsByCountryAndCategoryName(String country, @Param("categoryName") String category, Pageable pageable);
     @Query("SELECT r FROM Restaurants r WHERE r.latitude BETWEEN :latMax AND :latMin AND r.longitude BETWEEN :longMax AND :longMin")
     Page<Restaurants> findRestaurantsByLatitudeAndLongitudeBetween(@Param("latMin") double latMin, @Param("latMax") double latMax, @Param("longMin") double longMin, @Param("longMax") double longMax, Pageable pageable);
-    @Query("SELECT r.name FROM Restaurants r WHERE r.id = :restaurantId")
+    @Query("SELECT r.name FROM Restaurants r WHERE r.restaurantId = :restaurantId")
     String findNameByRestaurantId(@Param("restaurantId") Integer restaurantId);
+    @Query(value = "SELECT * FROM Restaurants WHERE description LIKE %:keyword% ", nativeQuery = true)
+    Page<Restaurants> findRestaurantsByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
+
+//建立搜尋索引
+//CREATE INDEX idx_restaurant_fulltext
+//ON restaurants
+//USING gin(to_tsvector('simple', name || ' ' || description || ' ' || country || ' '||district||' ' ||address));
