a259d72de87e09ac1d89449fe498286f884a0727
djspchen <djspchen@gmail.com>
2025-02-05 15:37:14 +0800

    couponOrder add method

--- src/main/java/com/richfood/controller/CouponsOrdersController.java
+++ src/main/java/com/richfood/controller/CouponsOrdersController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +18,7 @@ import com.richfood.service.CouponsOrdersService;
 import com.richfood.service.LinePayService;
 
 import jakarta.servlet.http.HttpServletRequest;
-
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
 @RequestMapping("/couponsOrder")
 public class CouponsOrdersController {
